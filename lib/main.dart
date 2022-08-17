fa56fbf451e0e4372a7437726fc2e5b2510b10c9
muhd-rashid <rashid.sulaiman93@gmail.com>
2022-08-17 18:33:47 +0800

    add tap interactivity, navigation stack and change app theme

--- lib/main.dart
+++ lib/main.dart
@@ -12,15 +12,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Applab',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Random Word Generator'),
-        ),
-        body: Center(
-          child: RandomWords(),
+      title: 'Apple',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black
         ),
       ),
+      home: RandomWords()
     );
   }
 }
@@ -34,22 +33,60 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return const Divider();
-          final index = i ~/ 2;
-          if (index >= _suggestions.length) {
-        _suggestions.addAll(generateWordPairs().take(10));
-      }
-          return ListTile(title: Text(
-        _suggestions[index].asPascalCase,
-        style: _biggerFont,
-      ),);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Random Text Generator'),
+        actions: [IconButton(onPressed: _pushSaved, icon: const Icon(Icons.list), tooltip: 'Saved Suggestions',)]),
+      body: ListView.builder(
+          padding: const EdgeInsets.all(16.0),
+          itemBuilder: (context, i) {
+            if (i.isOdd) return const Divider();
+            final index = i ~/ 2;
+            if (index >= _suggestions.length) {
+              _suggestions.addAll(generateWordPairs().take(10));
+            }
+            final alreadySaved = _saved.contains(_suggestions[index]);
+            return ListTile(
+              title: Text(
+                _suggestions[index].asPascalCase,
+                style: _biggerFont,
+              ),
+              trailing: Icon(
+                alreadySaved? Icons.favorite : Icons.favorite_border,
+                color: alreadySaved? Colors.red : null,
+                semanticLabel: alreadySaved? 'Remove from saved' : 'Save',
+              ),
+              onTap: () {
+                setState(() {
+                  if (alreadySaved) { _saved.remove(_suggestions[index]); }
+                  else { _saved.add(_suggestions[index]); }
+                });
+              },
+            );
+          })
+    );
+  }
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(builder: (context) {
+        final tiles = _saved.map((pair) { 
+          return ListTile(
+            title: Text(pair.asPascalCase, style: _biggerFont)
+          );
         });
+        final divided = tiles.isNotEmpty? ListTile.divideTiles(
+          context: context, tiles: tiles).toList() : <Widget>[];
+
+        return Scaffold(
+          appBar: AppBar(title: const Text('Saved Suggestions')),
+          body: ListView(children: divided),
+        );
+      })
+    );
   }
 }
 
